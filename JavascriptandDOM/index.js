221d64deaafb66fc2e4c8a82d6430d843a8e9cda
WatchDogg27 <96317430+WatchDogg27@users.noreply.github.com>
2022-05-23 17:04:36 +0100

    Add files via upload

---
+++ JavascriptandDOM/index.js
@@ -0,0 +1,71 @@
+//Write a series of conditional statements that:
+
+//• Prints "not a group" if musicians is less than or equal to 0
+
+let musicians = -2
+if (musicians <= 0){
+    print = "not a group"
+}
+
+
+
+
+
+//• Prints "solo" if musicians is equal to 1
+
+let musicians1 = 0
+if (musicians1 == 1){
+    print = "solo"
+}
+
+
+
+
+
+//• Prints "duet" if musicians is equal to 2
+
+let musicians2 = 2
+while (musicians2 == 2){
+    print = "duet";
+    musicians2++;
+}
+
+
+
+
+
+//• Prints "trio" if musicians is equal to 3
+
+var musicians3 = 3
+while (musicians3 == 3){
+    print = "trio";
+    musicians3++;
+}
+
+
+
+
+
+//• Prints "quartet" if musicians is equal to 4
+
+let musicians4 = -3
+
+switch(musicians4){
+    case 4 :
+        print = "quartet";
+}
+
+
+
+
+
+//• Prints "this is a large group" if musicians is greater than 4
+
+var musicians5 = 20
+if (musicians5 > 4){
+    print = "this is a large group"
+}
+
+
+
+
